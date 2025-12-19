d8844b3915e4836668e4bcfbd8315d6caf7e8595
unknown <faiznovalfajri2@gmail.com>
2025-12-19 21:12:10 +0700

    Upload 1

--- src/components/Cardprofile.jsx
+++ src/components/Cardprofile.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Cardprofile = () => {
+
+  const [togle, setTogle] = useState(false)
+
+  function funTogle() {
+    return setTogle(prev => !prev);
+  }
+
+
+
   return (
-    <div className='w-[538px] h-[717px] flex justify-center items-center flex-col px-[65px] border border-gray-400 rounded-[50px] bg-white gap-9'>
+    <div className={`w-[538px] h-[717px] relative flex justify-center items-center flex-col px-[65px] border border-gray-400 rounded-[50px] gap-9 ${togle ? "bg-[#252525] text-white" : "bg-white border border-gray-400"}`}>
+        
+        <button onClick={funTogle} className={`w-[180px] py-2 absolute -top-20 -right-3 rounded-md  text-xl cursor-pointer ${togle ? "border text-black" : "bg-[#252525] text-white"}`}>{togle ? "Light" : "Dark"}</button>
+        
         <img width={200} height={200} className="rounded-full" src="/public/foto profil.jpg" alt="foto profil" />
 
         <div className='w-full flex items-center flex-col text-center gap-6'>
